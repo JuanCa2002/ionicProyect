6e80d384524de4a7bb8f36f0fda1eaa1cc33c85f
Juan Camilo Torres Beltran <torres.juan.3055@eam.edu.co>
2023-03-10 14:10:29 -0500

    Primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  
   constructor() {}
+
+  personas = [
+     {
+        nombre:"Juan",
+        apellido: "Torres",
+        edad: 74
+      }
+    
+    ]
 
 }
